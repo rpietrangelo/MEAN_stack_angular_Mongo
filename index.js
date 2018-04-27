77d0add162815b4de3486b22a0c08ac146f12e0a
rpietrangelo <roccop1992@gmail.com>
2018-04-26 23:08:15 -0400

    server-setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const path = require('path');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+    if(err) {
+        console.log('Could not connect to the database!');
+    }
+    else{
+        console.log('Successfully connected to' + config.db +  '!')
+    }
+});
+
+app.use(express.static(__dirname + '/client/dist/'));
+
+app.get('*', (req, res) => {
+    res.sendFile(Path.join(__dirname + '/client/dist/index.html'));
+});
+
+//http://localhost:8080
+app.listen(8080, () =>{
+    console.log('Listening on Port 8080!');
+});
